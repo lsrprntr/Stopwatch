4367ee571db114d83d1d2a2608bfd5c247b0483f
Sams Sutio <39038103+lsrprntr@users.noreply.github.com>
2024-01-29 11:32:09 +1100

    basic commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Stopwatch
 {
-    partial class Form1
+    partial class Stopwatch
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,87 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.watchLabel = new System.Windows.Forms.Label();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.stopbutton = new System.Windows.Forms.Button();
+            this.resetbutton = new System.Windows.Forms.Button();
+            this.formTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // watchLabel
+            // 
+            this.watchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.watchLabel.Location = new System.Drawing.Point(12, 102);
+            this.watchLabel.Name = "watchLabel";
+            this.watchLabel.Size = new System.Drawing.Size(471, 122);
+            this.watchLabel.TabIndex = 0;
+            this.watchLabel.Text = "00:00.00";
+            this.watchLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // startbutton
+            // 
+            this.startbutton.BackColor = System.Drawing.Color.White;
+            this.startbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startbutton.Location = new System.Drawing.Point(44, 256);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(130, 87);
+            this.startbutton.TabIndex = 1;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = false;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // stopbutton
+            // 
+            this.stopbutton.BackColor = System.Drawing.Color.White;
+            this.stopbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopbutton.Location = new System.Drawing.Point(180, 256);
+            this.stopbutton.Name = "stopbutton";
+            this.stopbutton.Size = new System.Drawing.Size(130, 87);
+            this.stopbutton.TabIndex = 2;
+            this.stopbutton.Text = "Stop";
+            this.stopbutton.UseVisualStyleBackColor = false;
+            this.stopbutton.Click += new System.EventHandler(this.stopbutton_Click);
+            // 
+            // resetbutton
+            // 
+            this.resetbutton.BackColor = System.Drawing.Color.White;
+            this.resetbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetbutton.Location = new System.Drawing.Point(316, 256);
+            this.resetbutton.Name = "resetbutton";
+            this.resetbutton.Size = new System.Drawing.Size(130, 87);
+            this.resetbutton.TabIndex = 3;
+            this.resetbutton.Text = "Reset";
+            this.resetbutton.UseVisualStyleBackColor = false;
+            this.resetbutton.Click += new System.EventHandler(this.resetbutton_Click);
+            // 
+            // formTimer
+            // 
+            this.formTimer.Interval = 10;
+            this.formTimer.Tick += new System.EventHandler(this.formTimer_Tick);
+            // 
+            // Stopwatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(495, 450);
+            this.Controls.Add(this.resetbutton);
+            this.Controls.Add(this.stopbutton);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.watchLabel);
+            this.Name = "Stopwatch";
+            this.Text = "Stopwatch";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label watchLabel;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Button stopbutton;
+        private System.Windows.Forms.Button resetbutton;
+        private System.Windows.Forms.Timer formTimer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,45 @@ using System.Windows.Forms;
 
 namespace Stopwatch
 {
-    public partial class Form1 : Form
+    public partial class Stopwatch : Form
     {
-        public Form1()
+
+        private DateTime startTime;
+
+        public Stopwatch()
         {
             InitializeComponent();
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            // Sets start time
+            startTime = DateTime.Now;
+
+            // Start formTimer
+            formTimer.Start(); 
+
+        }
+
+        private void stopbutton_Click(object sender, EventArgs e)
+        {
+            formTimer.Stop();
+
+        }
+
+        private void resetbutton_Click(object sender, EventArgs e)
+        {
+            // Reset Label and stop timer
+            formTimer.Stop();
+            watchLabel.Text = "00:00.00";
+
+        }
+
+        private void formTimer_Tick(object sender, EventArgs e)
+        {
+            // Calculate time elapsed
+            TimeSpan ts = DateTime.Now - startTime;
+            watchLabel.Text = ts.ToString(@"mm\:ss\.ff");
         }
     }
 }
